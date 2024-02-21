1369cc41fd3d9a1811c7e51b6df0bd493fbddbe9
alionaterguta <71376066+alionaterguta@users.noreply.github.com>
2024-02-21 14:34:37 +0100

    fetch data from API

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -1,42 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: 'La Femme Nikita',
-      description: 'Is a 1990 French-language action thriller film written and directed by Luc Besson.A teen criminal who is convicted and sentenced to life imprisonment for murdering policemen during an armed pharmacy robbery. Her government handlers fake her death and recruit her as a professional assassin. After intense training, she starts a career as a killer, where she struggles to balance her work with her personal life. She shows talent at this and her career progresses until a mission in an embassy goes awry.',
-      genre: [ 'Action' ],
-      director: 'Luc Besson',
-      image: 'https://media.themoviedb.org/t/p/original/owzoJZoIIRgRyAbr8rx78wkCJaj.jpg',
-      featured: true
+  const [movies, setMovies] = useState([]);
 
-    },
-    {
-      id: 2,
-      title: 'Interstellar',
-      description: 'The movie follows a team of astronauts who embark on a space mission through a wormhole near Saturn to find a new habitable planet for humanity, as Earth faces environmental collapse. Interweaving elements of theoretical physics and emotional storytelling, the film explores themes of love, time dilation, and the survival of the human race.',
-      genre: [ 'Science Fiction' ],
-      director: 'Christopher Nolan',
-      image: 'https://media.themoviedb.org/t/p/original/bzONet3OeCTz5q9WOkGjVpOHMSR.jpg',
-      featured: true
-    },
-    {
-      id: 3,
-      title: 'The Prestige',
-      description: 'Two stage magicians engage in competitive one-upmanship in an attempt to create the ultimate.',
-      genre: [ 'Thriller' ],
-      director: 'Christopher Nolan', 
-      image: 'https://media.themoviedb.org/t/p/original/a03qYIZM0zkEutN5JRZddTBmQqY.jpg',
-      featured: true
-    }
-  ]);
+  const [directors, setDirectors] = useState([]);
+  useEffect(()  => {
+    fetch("https://cine-verse-b8832aa84c3e.herokuapp.com/movies/directors")
+      .then((response) => response.json())
+      .then((data) => {
+        const directorsFromApi = data.map((director) => {
+            return {
+              id: director._id,
+              name: director.Name
+            };
+         });
+      setDirectors(directorsFromApi)
+      })
+  }, []);
 
-  const [selectedMovie, setSelectedMovie] =useState(null);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://cine-verse-b8832aa84c3e.herokuapp.com/movies")
+    .then((response) => response.json())
+    .then((data) => {
+      const moviesFromApi = data.map((movie) => {
+        return {
+          id: movie.key,
+          title: movie.Title,
+          genre: movie.Genre,
+          description: movie.Description,
+          director: movie.Director
+          
+        };
+      });
+      setMovies(moviesFromApi);
+      // console.log( 'hdfsgfd', directors);
+
+    });
+  }, []);
 
   if (selectedMovie) {
+    const directorName = directors.filter((director) => director.id === selectedMovie.director);
+      selectedMovie.director = directorName[0].name;
+    
     return(
       <MovieView 
       movie={selectedMovie}
@@ -54,7 +63,7 @@ export const MainView = () => {
     <div> 
       {movies.map((movie) =>(
         <MovieCard
-        key={movie.id}
+        key={movie._id}
         movie={movie}
         onMovieClick={(newSelectedMovie) => {
           setSelectedMovie(newSelectedMovie);
