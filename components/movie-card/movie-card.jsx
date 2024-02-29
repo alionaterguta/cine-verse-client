43d58228e451093a082910cc4d73aafb178421d1
alionaterguta <71376066+alionaterguta@users.noreply.github.com>
2024-02-29 16:56:42 +0100

    edit Button

--- components/movie-card/movie-card.jsx
+++ components/movie-card/movie-card.jsx
@@ -1,18 +1,20 @@
+import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap"
+import { Link } from "react-router-dom";
 
 //destructered prop - movie is the name of the prop in main-view.jsx
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
     <Card>
       <Card.Img variant="top" src={movie.image} />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
         <Card.Text>{movie.genre}</Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
 
-        <Button onClick={() => onMovieClick(movie)} variant="link">
-          Open
-        </Button>
+        <Button variant="link">Open</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
@@ -20,6 +22,6 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired
 };
