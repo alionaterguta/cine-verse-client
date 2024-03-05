fb849577c23a5ac4bd3e78dc50646cb62df8c275
alionaterguta <71376066+alionaterguta@users.noreply.github.com>
2024-03-05 11:59:57 +0100

    add function delete user account

--- components/profile-view/profile-view.jsx
+++ components/profile-view/profile-view.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { useEffect, useState } from "react";
-// import PropTypes from "prop-types";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col"
+// import 
+
+import { useEffect, useState } from "react";
 import { FavoriteMovies } from "./favorite-movies";
 import { UpdateUser }  from "./update-user";
-import { Card } from "react-bootstrap";
-// import Image from 'react-bootstrap/Image';
+import { Card, Button, Image} from "react-bootstrap";
+import Profile_img from "../../img/user_profile_icon.png";
+import "./profile-view.scss";
 
 export const ProfileView = ({ token, user, movies }) => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -21,9 +23,10 @@ export const ProfileView = ({ token, user, movies }) => {
   const formData = {
     UserName: username,
     Email: email,
-    Birthdate: birthdate,
     Password: password
   };
+
+  formData.Birthdate = new Date(birthdate).toISOString().substring(0, 10);
 
   const handleSubmit = (event) => {
     event.preventDefault(event);
@@ -40,6 +43,7 @@ export const ProfileView = ({ token, user, movies }) => {
      .then((response) => {
       if (response.ok) {
         alert("Update successful");
+        localStorage.clear();
         window.location.reload();
         return response.json()
       }
@@ -72,18 +76,53 @@ export const ProfileView = ({ token, user, movies }) => {
         default:
     }
 }
+
+const handleDeleteAccount = (id) => {
+  fetch (`https://cine-verse-b8832aa84c3e.herokuapp.com/users/${id}`, {
+    method: "DELETE",
+    headers: { Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json"
+    }
+}).then ((response) => {
+  if (response.ok) {
+    alert("The account has been successfully deleted.");
+    localStorage.clear();
+    window.location.reload();
+  } else {
+    alert("Something went wrong.");
+    }
+  });
+};
+
   
   return (
     <>
       <Row>
-        <Card>
-        {/* <Card.Img src="holder.js/171x180" roundedCircle /> */}
+        <Card >
+          <Row>
+            <Col />
+            <Col>
+            <img src={Profile_img}  
+             width="80"
+             height="80"         
+             className="profile-img" />
+            </Col>
+            <Col />
+          </Row>
         <Card.Body>
-        <Card.Title><h2> My Profile </h2></Card.Title>
+        <Col 
+        >
+        </Col>
+        <Card.Title><h2> Hello {username}! </h2></Card.Title>
         <Card.Text>
-          <h6>{username}</h6>
+          {/* <h6>{username}</h6> */}
           <h6>{email}</h6>
         </Card.Text>
+        <br />
+        <Button onClick={() => handleDeleteAccount(storedUser._id)} 
+        className="button-delete mt-3" 
+        type="submit" variant="outline-secondary">
+        Delete account</Button>
         </Card.Body>
         </Card>
         <Col>
@@ -96,10 +135,10 @@ export const ProfileView = ({ token, user, movies }) => {
         <br />
       </Row>
       <hr />
-       <Row>
-        <Col className="mb-5" xs={12} md={9}>
+       <Row className="justify-content-center">
+       
           <FavoriteMovies user={user} favoriteMovies={favoriteMovies} />
-        </Col>
+        
       </Row>
     </>
   ); 
